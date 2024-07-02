89a287e4852c4419e4c9f0834926bc4eb9317b48
Fábio Siqueira <fabiofaleiros@gmail.com>
2024-07-02 12:41:00 +0100

    feat(config): spring cloud config-server

---
+++ services/config-server/src/test/java/tech/ffs/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ffs.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
